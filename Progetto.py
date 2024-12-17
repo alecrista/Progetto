4eb537192acb243911711299d2548c198e8a2f76
Ale <alessandrocristanelli@gmail.com>
2024-12-17 20:09:01 +0100

    modified:   Progetto.py

--- Progetto.py
+++ Progetto.py
@@ -3,15 +3,13 @@ import altair as alt
 import polars as pl
 import datetime as dt
 import pandas as pd
+import math as mt
 data=pl.read_csv("https://raw.githubusercontent.com/nbugliar/motogp_regression/master/MotoGP_2005_2017.csv", null_values=[" ","NA","crash"])
 st.write("Nella colonna Position, dove manca il valore, è perché il pilota non ha terminato il Gran Premio.")
 st.write("PURTROPPO, PER RAGIONI A NOI IGNOTE, NEL DATASET MANCANO I RISULTATI DEL GP DI SPAGNA CLASSE 125cc. LE CLASSIFICHE DI QUELLA STAGIONE RISULTAERANNO QUINDI ALTERATE.")
 st.title("Database Motomondiale 2005-2017")
 st.header("Database completo da analizzare")
 st.write(data)
-#date=st.date_input("Inserire giorno:", dt.datetime(2020,1,5))
-#st.write(data.filter(pl.col("Date")==date))
-#st.write(data.select(pl.col("Date").cast(pl.Date)))
 st.header("Classifiche generali")
 col1, col2=st.columns(2)
 year=col1.select_slider("Inserire anno:", range(2005,2018), key=1)
@@ -29,10 +27,7 @@ costr=data.group_by("Year", "TRK", "Category", "Bike").agg(pl.col("Points").max(
 class_costr=costr.group_by("Year", "Category", "Bike").agg(pl.col("Tot_points").sum()).filter(pl.col("Year")==year, pl.col("Category")==cat).sort("Tot_points", descending=True)
 col2.subheader("Classifica costruttori")
 col2.write(class_costr)
-#d_prog=data.filter(pl.col("Year")==year, pl.col("Category")==cat).group_by("Year","Category","Rider_Name","Date").agg(pl.col("Points").sum())
-#st.write(d_prog)
-#chart=alt.Chart(d_prog).mark_line().encode(alt.X("Date"), alt.Y("Points"), alt.Color("Rider_Name"))
-#st.write(chart)
+
 ########
 st.header("Risultati di ogni GP per categoria")
 year1=st.select_slider("Inserire anno:", range(2005,2018), key=2)
@@ -60,17 +55,39 @@ else:
     st.write(d4)
 ########
 st.header("Analisi pilota per pilota")
-data_rider_points=data.group_by("Rider_Name","Year","Category","Bike").agg(pl.col("Points").sum())
-st.write(data_rider_points)
-#col1,col2,col3=st.columns(3)
-#track=col2.multiselect("Inserire Paese:", ["QAT","ARG","AME","SPA","ARA"])
-#cat=col3.multiselect("Inserire Categoria:", ["MotoGP","Moto2","Moto3","250cc","125cc"])
-#col11,col21,col31,col41=st.columns(4)
-#ses=col11.multiselect("Inserire sessione (RAC: gara, RAC2: ripartenza):", ["RAC","RAC2"])
-#tcon=col21.multiselect("Inserire condizioni pista:", ["Dry", "Wet"])
-#ttemp=col31.select_slider("Inserire temperatura asfalto:", range(20,60))
-#atemp=col41.select_slider("Inserire temperatura aria:", range(15,40))
-#points=st.select_slider("Inserire punteggio:", [0,1,2,3,4,5,6,7,8,9,10,11,13,16,20,25])
-#st.write(data.filter(pl.col("Year")==year,pl.col("TRK").is_in(track), pl.col("Category").is_in(cat),
-#                     pl.col("Session").is_in(ses),pl.col("Track_Condition").is_in(tcon),pl.col("Track_Temp")==ttemp,
-#                     pl.col("Air_Temp")==atemp,pl.col("Points")==points))
+st.subheader("Piloti e punti conquistati")
+data_rider_points=data.group_by("Rider_Name","Year","Category","Bike").agg(pl.col("Points").sum()).sort("Rider_Name", "Year", descending=True)
+rider_list=data_rider_points["Rider_Name"].unique().to_list()
+cat_list=data_rider_points["Category"].unique().to_list()
+col111, col112=st.columns(2)
+riders=col111.multiselect("Inserire piloti desiderati:", rider_list)
+category=col111.multiselect("Inserire Categoria desiderata:", cat_list)
+col112.write(data_rider_points.filter(pl.col("Rider_Name").is_in(riders), pl.col("Category").is_in(category)))
+st.subheader("Piloti e velocità dura e pura")
+data_rider_speed=data.group_by("Rider_Name","Year","Category","Bike","Track_Condition").agg(
+    pl.col("Avg_Speed").mean()).sort("Rider_Name", "Year", descending=True).filter(pl.col("Avg_Speed")>0) #Così rimuovo i dati assenti
+st.write("*Pista asciutta*")
+mud=data_rider_speed.filter(pl.col("Track_Condition")=="Dry").select(pl.col("Avg_Speed").mean())[0,0]#più conveniente di numpy
+var=data_rider_speed.filter(pl.col("Track_Condition")=="Dry").select(pl.col("Avg_Speed").var())[0,0] #idem della riga 70
+n=len(data_rider_speed.filter(pl.col("Track_Condition")=="Dry")["Rider_Name"].to_list())
+st.write("Velocità media ti tutti i piloti:", mud)
+ic=[mud-1.96*mt.sqrt(var*n/(n-1)), mud+1.96*mt.sqrt(var*n/(n-1))]
+st.write("Intrvallo di confidenza a livello della velocità media, calcolato con l'opportuna formula mu +- 1.96 x sqrt(var x n/(n-1)):", ic)
+st.write("Procedimento utilizzato per escludere valori poco significativi o poco rilevanti.")
+st.write(data_rider_speed.filter(pl.col("Track_Condition")=="Dry").filter(pl.col("Avg_Speed")>=ic[0]).filter(pl.col("Avg_Speed")<=ic[1]))
+st.write("*Pista bagnata*")
+mud=data_rider_speed.filter(pl.col("Track_Condition")=="Wet").select(pl.col("Avg_Speed").mean())[0,0]#più conveniente di numpy
+var=data_rider_speed.filter(pl.col("Track_Condition")=="Wet").select(pl.col("Avg_Speed").var())[0,0] #idem della riga 70
+n=len(data_rider_speed.filter(pl.col("Track_Condition")=="Wet")["Rider_Name"].to_list())
+st.write("Velocità media ti tutti i piloti:", mud)
+ic=[mud-1.96*mt.sqrt(var*n/(n-1)), mud+1.96*mt.sqrt(var*n/(n-1))]
+st.write("Intrvallo di confidenza a livello della velocità media, calcolato con l'opportuna formula mu +- 1.96 x sqrt(var x n/(n-1)):", ic)
+st.write("Procedimento utilizzato per escludere valori poco significativi o poco rilevanti.")
+st.write(data_rider_speed.filter(pl.col("Track_Condition")=="Wet").filter(pl.col("Avg_Speed")>=ic[0]).filter(pl.col("Avg_Speed")<=ic[1]))
+
+######
+st.header("Analisi per date")
+col01, col02, col03=st.columns(3)
+data_amg=st.date_input("Inserire data:", min_value=dt.date(2005, 1, 1), max_value=dt.date(2017, 12, 31))
+data_date=data.with_columns(pl.col("Date").str.strptime(pl.Date, "%Y-%m-%d").alias("PyDate"))
+st.write(data_date.filter(pl.col("PyDate")==data_amg))
